5e7230c0688cc6b4a95eab1bb239a975d942a954
Tineni Baeyens <btineni@gmail.com>
2024-10-08 14:42:05 +0200

    rajout d'un loading dans l'index

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 let animationID;
-
 const dvdText = document.getElementById("dvdText");
-/* document.body.style.backgroundColor = "black";   */
+const loadingText = document.getElementById("loadingText");
 const colors = ["red", "green", "blue", "pink", "orange", "purple"];
 
 function loop() {
-const hRange = window.innerWidth - dvdText.clientWidth;
-const vRange = window.innerHeight - dvdText.clientHeight;
-const time = performance.now() * 0.2;
+    const hRange = window.innerWidth - dvdText.clientWidth;
+    const vRange = window.innerHeight - dvdText.clientHeight;
+    const time = performance.now() * 0.2;
 
-const x = Math.abs((time % (hRange * 2)) - hRange);
-dvdText.style.left = `${x}px`;
-const y = Math.abs((time % (vRange * 2)) - vRange);
-dvdText.style.top = `${y}px`;
+    const x = Math.abs((time % (hRange * 2)) - hRange);
+    dvdText.style.left = `${x}px`;
+    const y = Math.abs((time % (vRange * 2)) - vRange);
+    dvdText.style.top = `${y}px`;
 
-const bounces = Math.floor(time / hRange) + Math.floor(time / vRange)
-   dvdText.style.color = colors[bounces % colors.length]
+    const bounces = Math.floor(time / hRange) + Math.floor(time / vRange);
+    dvdText.style.color = colors[bounces % colors.length];
 
-animationID = requestAnimationFrame(loop);
-}  
+    animationID = requestAnimationFrame(loop);
+}
 
 loop();
 
-// Arrêter l'animation et rediriger après 10 secondes
+// Stop the animation and redirect after 10 seconds
 setTimeout(() => {
     cancelAnimationFrame(animationID);
-    window.location.href = "mainpage/main.html";
-    ; // Remplace par l'URL de ton portfolio
+    window.location.href = "mainpage/main.html"; // Replace with your portfolio URL
 }, 6500);
